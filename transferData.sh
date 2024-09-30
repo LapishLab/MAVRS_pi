091bf59b5f3b8e1a77d185e48ace525dceb820ba
davidswygart <davidswygart@gmail.com>
2024-09-30 19:39:54 -0400

    Create transferData.sh

---
+++ transferData.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# requires username as 1st positional argument and path to save folder as 2nd positional argument
+
+cd $(dirname "$(readlink -f "$0")")
+path=$1@10.1.1.0:$2
+echo "Started data transfer to ""$path"" at ""$(date +%H:%M:%S)"" "
+rsync --recursive --times --compress --progress --exclude=".*"  data/ "$path"
