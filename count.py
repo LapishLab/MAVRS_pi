0414da11072ef725444c02c897de3571d14e6b63
David Swygart <davidswygart@gmail.com>
2026-05-17 12:48:40 -0400

    switch to multiprocessing

--- count.py
+++ count.py
@@ -2,27 +2,33 @@ import time
 from pathlib import Path
 from argparse import ArgumentParser
 
-#Parse recording settings
-parser = ArgumentParser(description='Test script')
-parser.add_argument(
-	'--saveDir', 
-	help='Path within the Data folder to which data will be saved'
-)
-parser.add_argument(
-	'--fname',
-	help='Filename to save data to (without extension)'
-)
-args = parser.parse_args()
 
-saveDir = Path(args.saveDir)
-saveDir.mkdir(parents=True, exist_ok=True)
-saveFile = saveDir / f'{args.fname}.txt'
-print('SavingFile as ' + str(saveFile))
+def main(saveDir, fname):
+	saveDir = Path(saveDir)
+	saveDir.mkdir(parents=True, exist_ok=True)
+	saveFile = saveDir / f'{fname}.txt'
+	print('SavingFile as ' + str(saveFile))
 
-i = 0
-while True:
-	print(f'Writing {i}')
-	with open(saveFile, "a", newline='') as f: 
-		f.write(str(i))
-	i+=1
-	time.sleep(1)
+	i = 0
+	while True:
+		print(f'Writing {i}')
+		with open(saveFile, "a", newline='') as f:
+			f.write(str(i))
+		i += 1
+		time.sleep(1)
+
+
+if __name__ == '__main__':
+	# Parse recording settings when run as a script
+	parser = ArgumentParser(description='Test script')
+	parser.add_argument(
+		'--saveDir', 
+		help='Path within the Data folder to which data will be saved'
+	)
+	parser.add_argument(
+		'--fname',
+		help='Filename to save data to (without extension)'
+	)
+	args = parser.parse_args()
+
+	main(args.saveDir, args.fname)
--- startExperiment.py
+++ startExperiment.py
@@ -1,41 +1,54 @@
 from argparse import ArgumentParser
 from datetime import datetime
-from subprocess import Popen
+from multiprocessing import Process
+import count
 from config import DATA_DIR, ROOT_DIR, HOSTNAME
 import signal
 import time
 import sys
 
-parser = ArgumentParser(description='start an experiment')
-parser.add_argument('--session', help='SessionName')
-args = parser.parse_args()
 
-if args.session is None:
-	args.session = datetime.now().strftime("%Y%m%d_%H%M%S")
+def main():
+	parser = ArgumentParser(description='start an experiment')
+	parser.add_argument('--session', help='SessionName')
+	args = parser.parse_args()
 
-saveDir = DATA_DIR / args.session / HOSTNAME
+	if args.session is None:
+		args.session = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-print('starting experiment')
+	saveDir = DATA_DIR / args.session / HOSTNAME
 
-processes = []
+	print('starting experiment')
 
-# Start count recording
-processes.append(Popen(['python', '-u', ROOT_DIR/'count.py', '--saveDir', saveDir, '--fname', 'count_A']))
-processes.append(Popen(['python', '-u', ROOT_DIR/'count.py', '--saveDir', saveDir, '--fname', 'count_B']))
+	processes = []
 
+	# Start count recording using multiprocessing
+	processes.append(Process(target=count.main, args=(saveDir, 'count_A')))
+	processes.append(Process(target=count.main, args=(saveDir, 'count_B')))
 
-def handle_sigterm(signum, frame):
-	print("\nCleaning up subprocesses...")
-	for p in processes: 
-		if p.poll() is None: 
-			p.terminate()
 	for p in processes:
-		p.wait() 
-	sys.exit(0)
+		p.start()
 
-# Register the SIGTERM handler
-signal.signal(signal.SIGTERM, handle_sigterm)
-signal.signal(signal.SIGINT, handle_sigterm) #SIGINT is for handling Ctrl+C gracefully
 
-while True:
-	time.sleep(1)
+	def handle_sigterm(signum, frame):
+		print("\nCleaning up processes...")
+		for p in processes:
+			if p.is_alive():
+				p.terminate()
+		for p in processes:
+			p.join()
+		sys.exit(0)
+
+	# Register the SIGTERM handler
+	signal.signal(signal.SIGTERM, handle_sigterm)
+	signal.signal(signal.SIGINT, handle_sigterm)  # SIGINT is for handling Ctrl+C gracefully
+
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		handle_sigterm(None, None)
+
+
+if __name__ == '__main__':
+	main()
